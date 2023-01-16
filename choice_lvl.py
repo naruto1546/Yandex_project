3168862fa3ecdedc201c5aa01b60ed3b28e3cd93
naruto <121440906+naruto1546@users.noreply.github.com>
2023-01-17 01:31:49 +0300

    Add files via upload

--- choice_lvl.py
+++ choice_lvl.py
@@ -4,20 +4,26 @@ from button import Button
 
 class ChoiceLvl:
     def __init__(self):
-        def fn1():
+        def lvl1():
             pass
 
-        button_choice_lvl_1 = Button(position=(80, 100), size=(100, 50),
-                                     clr=(220, 220, 220),
-                                     cngclr=(255, 0, 0), func=fn1,
+        def lvl2():
+            pass
+
+        def lvl3():
+            pass
+
+        button_choice_lvl_1 = Button(position=(100, 100), size=(100, 50),
+                                     clr='#34C924',
+                                     cngclr=(255, 0, 0), func=lvl1,
                                      text='Уровень 1')
-        button_choice_lvl_2 = Button(position=(80, 100), size=(100, 50),
-                                     clr=(220, 220, 220),
-                                     cngclr=(255, 0, 0), func=fn1,
+        button_choice_lvl_2 = Button(position=(100, 160), size=(100, 50),
+                                     clr='#34C924',
+                                     cngclr=(255, 0, 0), func=lvl2,
                                      text='Уровень 2')
-        button_choice_lvl_3 = Button(position=(80, 100), size=(100, 50),
-                                     clr=(220, 220, 220),
-                                     cngclr=(255, 0, 0), func=fn1,
+        button_choice_lvl_3 = Button(position=(100, 220), size=(100, 50),
+                                     clr='#34C924',
+                                     cngclr=(255, 0, 0), func=lvl3,
                                      text='Уровень 3')
         self.buttons = [button_choice_lvl_1, button_choice_lvl_2,
                         button_choice_lvl_3]
